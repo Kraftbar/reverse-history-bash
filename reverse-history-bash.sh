5d759b16b13bddaf3e7ab9970e7ebbafc9bf057a
Gaute <gautenybo@gmail.com>
2023-10-07 19:16:29 +0200

    Update reverse-history-bash.sh

--- reverse-history-bash.sh
+++ reverse-history-bash.sh
@@ -1,5 +1,9 @@
+# ---------- Manage Cursor ----------
+HISTORY_FILE="~/.bash_history"
+PROMPT_COLUMN=25
+OFFSET=22
 
-extract_current_cursor_position () {
+extract_current_cursor_position() {
     export $1
     exec < /dev/tty
     oldstty=$(stty -g)
@@ -8,66 +12,74 @@ extract_current_cursor_position () {
     IFS=';' read -r -d R -a pos
     stty $oldstty
     eval "$1[0]=$((${pos[0]:2} - 2))"
-    eval "$1[1]=$((${pos[1]} - 1))"
+    eval "$1[1]=$((${pos[1]} - 2))"
 }
-colstot=$(tput lines)
-totrows=$(tput cols)
 
+adjust_cursor_position() {
+    local diff=$(( $(tput lines) - ${pos1[0]} ))
 
-# Save the current history to a file
-history -a ~/.bash_history
+    if [ $diff -lt 5 ]; then
+        tput cup $((pos1[0] + diff - 6)) $(($PROMPT_COLUMN + $OFFSET))
+        pos1[0]=$((pos1[0] + diff - 6))
+        pos1[1]=$(($PROMPT_COLUMN + $OFFSET))
+    else
+        tput cup $((${pos1[0]} - 1)) $(($PROMPT_COLUMN + $OFFSET))
+        pos1[0]=$((pos1[0] + 1))
+        pos1[1]=$(($PROMPT_COLUMN + $OFFSET))
+    fi
+}
 
-# Search for a fuzzy match of a given string in the history file
-# todo: if the string is over totrows or over several lines then cut the string and add ... at the end 
+# ---------- User Interface Functions ----------
+
+display_prompt() {
+    tput cup $((${pos1[0]} - 1)) $PROMPT_COLUMN
+    echo "Enter search string: "
+    echo -e "\n\n\n "
+}
+
+capture_user_input() {
+    while read -r -n 1 char; do
+        if [ "$char" = $'\x7f' ]; then
+            search_string="${search_string%?}"
+        else
+            search_string="$search_string$char"
+        fi
+
+        display_search_results
+        reposition_cursor
+
+        # Move the cursor forward here, after repositioning it
+        if [ "$char" != $'\x7f' ]; then
+            tput cuf1
+        fi
+    done
+}
+
+# ---------- Search Logic ----------
+
 fuzzy_search() {
-  local search_string="$1"
-  local history_file="$2"
-  local matches
-
-  matches=$(grep -i "$search_string" "$history_file" | awk '{print $0}')
-
-  echo "$matches" | tail -5
+    local search_string="$1"
+    local history_file="$2"
+    local matches=$(grep -i "$search_string" "$history_file" | awk '{print $0}')
+    echo "$matches" | tail -5
 }
 
+display_search_results() {
+    fuzzy_search "$search_string" ~/.bash_history
+}
 
-# Use the fuzzy_search function to search the history file for a string
-# entered by the user, updating the match at every character written
+reposition_cursor() {
+    tput cup $((${pos1[0]} - 2)) $(($PROMPT_COLUMN + $OFFSET + ${#search_string} - 1))
+    echo     $((${pos1[0]} - 2)) $(($PROMPT_COLUMN + $OFFSET + ${#search_string} - 1)) >> test.log
+}
+
+# ---------- Main Program ----------
+
+history -a ~/.bash_history
 search_string=""
 extract_current_cursor_position pos1
-pos1[0]=$((pos1[0]+1))
-tput cup ${pos1[0]} 27
-diff=$((colstot - pos1[0]))
-
-# todo: sub make $((27 + 21)) not hardcoded
-echo  "Enter search string: "
-echo  -e "\n\n\n "
-echo $diff >> test.log
-if [ $diff -lt 5 ]; then
-  tput cup $((pos1[0] + diff-6)) $((27 + 21))
-  pos1[0]=$((pos1[0] + diff-6))
-  pos1[1]=$((27 + 21))
-else
-  tput cup ${pos1[0]-1} $((27 + 21))
-  pos1[0]=$((pos1[0] +1))
-  pos1[1]=$((27 + 21))
-fi
-
-
-
-while read -r -n 1 char; do
-
-  if [ "$char" = $'\x7f' ]; then
-    # Handle backspace by removing the last character from the search string
-    search_string="${search_string%?}"
-    pos1[0]=$((pos1[0]-2))
-  else
-    # Append the entered character to the search string
-    search_string="$search_string$char"
-  fi
-
-  echo $search_string
-  fuzzy_search "$search_string" ~/.bash_history
-  tput cup $((${pos1[0]}-1)) $((27 + 21 +${#search_string}-1))
-
-done
+pos1[0]=$((pos1[0] + 1))
+display_prompt
+adjust_cursor_position
+capture_user_input
 
