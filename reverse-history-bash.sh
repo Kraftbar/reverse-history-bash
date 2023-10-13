8d188a21f657f10e0649445715a126f1ca75e1b7
Gaute <gautenybo@gmail.com>
2023-10-13 23:39:49 +0200

    Update reverse-history-bash.sh

--- reverse-history-bash.sh
+++ reverse-history-bash.sh
@@ -19,33 +19,40 @@ extract_current_cursor_position() {
 adjust_cursor_position() {
     local diff=$(( $(tput lines) - ${current_cursor_position[0]} ))
 
-    if [ $diff -lt 5 ]; then
-        tput cup $((current_cursor_position[0] + diff - 6)) $(($PROMPT_COLUMN + $OFFSET))
-        current_cursor_position[0]=$((current_cursor_position[0] + diff - 6))
-        current_cursor_position[1]=$(($PROMPT_COLUMN + $OFFSET))
-    else
         tput cup $((${current_cursor_position[0]}     - 1)) $(($PROMPT_COLUMN + $OFFSET))
         current_cursor_position[0]=$((current_cursor_position[0]        + 1))
         current_cursor_position[1]=$(($PROMPT_COLUMN + $OFFSET))
-    fi
 }
 
 # ---------- User Interface Functions ----------
 
 
-capture_user_input() {    ## active 
+capture_user_input() {
     while IFS= read -r -n 1 char; do
-    flush_screen
-        if [ "$char" = $'\x7f' ]; then
+        flush_screen
+        if [ "$char" = $'\x7f' ]; then  # Backspace
             search_string="${search_string%?}"
+        elif [ "$char" = $'\x0d' ]; then  # Enter
+            
+        elif [ "$char" = $'\x1b' ]; then  # Escape character
+            # Read the next two characters to complete the ANSI escape sequence
+            read -r -n 2 next_chars
+            escape_sequence="$char$next_chars"
+            if [ "$escape_sequence" = $'\x1b[A' ]; then  # Up arrow
+                # Increment or do something with current_cmd_index
+                current_cmd_index=$((current_cmd_index - 1))
+            elif [ "$escape_sequence" = $'\x1b[B' ]; then  # Down arrow
+                # Decrement or do something with current_cmd_index
+                current_cmd_index=$((current_cmd_index + 1))
+            fi
         else
             search_string="$search_string$char"
         fi
 
         display_search_results
         reposition_cursor
-        echo "sadad"$char"sda" >> test.log
-        echo $search_string >> test.log
+        echo "char: $char" >> test.log
+        echo "search string: $search_string" >> test.log
     done
 }
 
@@ -55,11 +62,29 @@ flush_screen(){
     echo -en "\e[J"
 }
 
+
+print_matches(){
+    string="$1"
+
+    count=0
+  echo "$string" | while IFS= read -r line; do
+    if [ "$current_cmd_index" -eq "$count" ]; then
+      echo ">$line"
+    else
+      echo " $line"
+    fi
+    count=$((count + 1))
+    [ "$count" -ge 5 ] && break
+  done
+}
+
 fuzzy_search() {   ## active
     local search_string="$1"
     local history_file="$2"
     local matches=$(grep -i "$search_string" "$history_file" | tac | awk '!x[$0]++' )
-    echo "$matches" | head -5
+ #   echo "$matches" | head -5
+    print_matches "$matches"
+
 }
 
 display_search_results() {  ## active 
